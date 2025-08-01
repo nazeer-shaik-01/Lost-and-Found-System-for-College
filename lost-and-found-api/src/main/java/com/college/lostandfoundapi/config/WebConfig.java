808c78f7435a7190f1d5b73b73e3f9b6539b834e
NAZEER\nazee <nazeershaik1312@gmail.com>
2025-08-01 18:45:40 +0530

    Initial commit

--- lost-and-found-api/src/main/java/com/college/lostandfoundapi/config/WebConfig.java
+++ lost-and-found-api/src/main/java/com/college/lostandfoundapi/config/WebConfig.java
@@ -4,12 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+@Configuration // <-- Make sure this annotation is present
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/images/**")
-                .addResourceLocations("classpath:/static/images/");
+        registry.addResourceHandler("/images/**") // <-- This is the URL path
+                .addResourceLocations("classpath:/static/images/"); // <-- This is the file system path
     }
 }
